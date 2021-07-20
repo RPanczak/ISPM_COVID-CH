128b00e03789bfffd5326774771a6e66c08e6576
Radek <r.panczak@gmail.com>
2021-07-20 17:43:31 +0200

    data update

--- data-raw/BAG-open/BAG.R
+++ data-raw/BAG-open/BAG.R
@@ -1,7 +1,7 @@
 # data from 
 # https://www.bag.admin.ch/bag/en/home/krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/novel-cov/situation-schweiz-und-international.html
 # updated
-# 2021-06-08
+# 2021-07-20
 
 set.seed(12345)
 library(readxl)
